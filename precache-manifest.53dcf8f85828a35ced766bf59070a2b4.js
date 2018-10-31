b83335b272338c322fc953fe745dc7e49a472d71
Eclegend <groshev@uchi.ru>
2018-10-31 20:24:33 +0300

    Updates

---
+++ precache-manifest.53dcf8f85828a35ced766bf59070a2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "f4fef67fafca2c909e18",
+    "url": "/react-test/static/js/main.f4fef67f.chunk.js"
+  },
+  {
+    "revision": "74ae57e66f9a8e0a6b01",
+    "url": "/react-test/static/js/1.74ae57e6.chunk.js"
+  },
+  {
+    "revision": "f4fef67fafca2c909e18",
+    "url": "/react-test/static/css/main.1a26c275.chunk.css"
+  },
+  {
+    "revision": "768e4d3c29a6579001fa82d2df6526b8",
+    "url": "/react-test/index.html"
+  }
+];
